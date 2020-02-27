de289b953836cd2bb9b096f2ad1ec8f92ab66449
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-27 19:07:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,27 @@ def adding_matz
         }
      }
      
-    
+     programmer_hash = 
+ 		{
+ 		    :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        },
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+     
+    programmer_hash
     
 end
 
